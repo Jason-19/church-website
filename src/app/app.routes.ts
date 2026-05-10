ea21b4b650975d28009bbc6d7ed65b3f6dd8c69d
Jason <nunez19jason@gmail.com>
2026-05-10 14:30:04 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about').then(m => m.About)
+    },
+    {
+        path: 'events',
+        loadComponent: () => import('./pages/events/events').then(m => m.Events)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact').then(m => m.Contact)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('church-website');
+  isProfileMenuOpen = false;
+  isMobileMenuOpen = false;
+
+  constructor() {
+    console.log(this.isProfileMenuOpen);
+    console.log(this.isMobileMenuOpen);
+  }
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
 }
